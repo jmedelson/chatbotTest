e6c95013b26ecb0fd6ee08b231e23d9d28b54ce0
jmedelson <65310914+jmedelson@users.noreply.github.com>
2022-01-14 18:09:39 -0800

    helper site framework added

---
+++ V1/helperSite/main.js
@@ -0,0 +1,19 @@
+let ws = new WebSocket("wss://lhek8s2pxc.execute-api.us-east-2.amazonaws.com/dev?app=helper");
+function pingServer(){
+    ws.send('requesting vote')
+}
+ws.onopen = function() {
+    console.log('Websocket connected')
+    // ws.send("Browser Websocket Open");
+}
+ws.onmessage = function (evt) { 
+    console.log("received data over websocket")
+    console.log(received)
+    var received = evt.data;
+    let parsed = JSON.parse(received)
+    console.log(parsed)
+
+ };
+ ws.onclose = function() { 
+    console.log("websocket closed")
+ };
